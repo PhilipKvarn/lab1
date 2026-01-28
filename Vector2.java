f93629988f5a1b429d42e54cb89e6e26d38e6a98
philip <phikva@chalmers.se>
2026-01-28 14:10:09 +0100

    feat: documented the Vector2 class

--- Vector2.java
+++ Vector2.java
@@ -7,15 +7,28 @@ public class Vector2 {
         this.y = y;
     }
 
+    /***
+     * Static method which returns the zero-vector
+     * @return returns a Vector2 object of the zero-vector
+     */
     static public Vector2 zero(){
         return new Vector2(0, 0);
     }
 
+    /***
+     * Adds two doubles onto the vector, one for the x-axis and one for the y-axis
+     * @param x Value to be added to the x-axis of this vector
+     * @param y Value to be added to the y-axis of this vector
+    */
     public void add(double x, double y){
         this.x += x;
         this.y += y;
     }
 
+    /**
+     * Rotates this vector by a angle in degrees.
+     * @param rotation the angle, in degrees, which the vector will rotate to.
+     */
     public void rotateByDeg(double rotation){
         double convert = Math.PI/180.00;
         double radian = rotation*convert;

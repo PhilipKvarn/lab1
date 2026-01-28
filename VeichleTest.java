d558a318ef5f9750795b985126c9a9c4ccd89cf6
JWik05 <wiklanderjakob@gmail.com>
2026-01-28 13:26:35 +0100

    fix: fixed saab tests

--- VeichleTest.java
+++ VeichleTest.java
@@ -11,51 +11,52 @@ public class VeichleTest {
         assertEquals("125", s.getEnginePower());
         assertEquals(2, s.getNrDoors());
         assertEquals("red", s.getColor());
-        s.currentSpeed = 2;
-        double speed = s.currentSpeed;
+        s.startEngine();
+        s.incrementSpeed(2);
+        double speed = s.getCurrentSpeed();
         s.decrementSpeed(1);
-        assertTrue(speed > s.currentSpeed);
-        speed = s.currentSpeed;
+        assertTrue(speed > s.getCurrentSpeed());
+        speed = s.getCurrentSpeed();
         s.incrementSpeed(1);
-        assertTrue(speed < s.currentSpeed);
+        assertTrue(speed < s.getCurrentSpeed());
     }
 
     @Test
-    public void accelerateTest(){
+    public void accelerateTest() {
         Veichle saab95 = new Saab95();
         Vector2 pos = saab95.getPosition();
         Vector2 zeroVector = new Vector2(0, 0);
-        assertEquals(pos.x,zeroVector.x);
-        assertEquals(pos.y,zeroVector.y);
+        assertEquals(pos.x, zeroVector.x);
+        assertEquals(pos.y, zeroVector.y);
         saab95.startEngine();
         assertEquals(saab95.getCurrentSpeed(), 0.1);
         saab95.move();
-        assertEquals(pos.x,0.1);
-        assertEquals(pos.y,zeroVector.y);
-        saab95.incrementSpeed(10); // 0.1 +  1.25*10 = 12.6
+        assertEquals(pos.x, 0.1);
+        assertEquals(pos.y, zeroVector.y);
+        saab95.incrementSpeed(10); // 0.1 + 1.25*10 = 12.6
         assertEquals(saab95.getCurrentSpeed(), 12.6);
         saab95.move();
         assertEquals(pos.x, 12.7);
-        saab95.decrementSpeed(6.08); // 12.6 - 1.25*6.08 = 5 
+        saab95.decrementSpeed(6.08); // 12.6 - 1.25*6.08 = 5
         assertEquals(saab95.getCurrentSpeed(), 5);
         saab95.move();
         assertEquals(pos.x, 17.7);
     }
 
     @Test
-    public void steeringTest(){
+    public void steeringTest() {
         Veichle saab95 = new Saab95();
         saab95.turnLeft();
         Vector2 dir = saab95.getDirection();
-        double x = Math.round(dir.x*1000.0)/1000.0;
-        double y = Math.round(dir.y*1000.0)/1000.0;
+        double x = Math.round(dir.x * 1000.0) / 1000.0;
+        double y = Math.round(dir.y * 1000.0) / 1000.0;
         assertEquals(0.985, x);
         assertEquals(-0.174, y);
         saab95.turnRight();
         saab95.turnRight();
         dir = saab95.getDirection();
-        x = Math.round(dir.x*1000.0)/1000.0;
-        y = Math.round(dir.y*1000.0)/1000.0;
+        x = Math.round(dir.x * 1000.0) / 1000.0;
+        y = Math.round(dir.y * 1000.0) / 1000.0;
         assertEquals(0.985, x);
         assertEquals(0.174, y);
     }

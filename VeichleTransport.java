b71b9255c7063851f240d592520aa499b47b7199
philip <phikva@chalmers.se>
2026-02-09 11:44:29 +0100

    la till Transport i Arvshierarkin

---
+++ VeichleTransport.java
@@ -0,0 +1,10 @@
+import java.awt.Color;
+
+public class VeichleTransport extends Truck {
+
+    public VeichleTransport(int nrDoors, double enginePower, Color color, String modelName){
+        super(nrDoors,enginePower,color,modelName);
+    }
+    
+
+}

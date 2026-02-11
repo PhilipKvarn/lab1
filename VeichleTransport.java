0fab33dd004e64ca9fa9347f57616526207039b3
philip <phikva@chalmers.se>
2026-02-11 16:00:23 +0100

    feat ensure that veichle transport cannot load VeichleTransport

--- VeichleTransport.java
+++ VeichleTransport.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 public class VeichleTransport extends Truck {
 
     Stack<Veichle> loadingStorage = new Stack<Veichle>();
-    
+    VeichleLoader<Veichle> parentLoader = new VeichleLoader<>(4);
 
     public VeichleTransport(int nrDoors, double enginePower, Color color, String modelName){
         super(nrDoors,enginePower,color,modelName);
@@ -12,6 +12,9 @@ public class VeichleTransport extends Truck {
     }
 
     public void LoadNewVeichle(Veichle veichle){
+        if (veichle instanceof VeichleTransport) {
+            return;
+        }
         loadingStorage.addElement(veichle);
         return;
     }
@@ -27,5 +30,4 @@ public class VeichleTransport extends Truck {
             super.startEngine();
         }
     }
-
 }
